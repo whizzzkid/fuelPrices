6ea9ddb79c2de9b9bd0d92e718e37a66abcd2c7a
whizzzkid <me@nishantarora.in>
2020-07-15 00:13:12 -0600

    Fixing builds

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ export default {
     input: 'src/index.ts',
     output: {
         file: 'dist/fuelPrice.js',
-        format: 'es',
+        format: 'cjs',
     },
     plugins: [
         typescript(),

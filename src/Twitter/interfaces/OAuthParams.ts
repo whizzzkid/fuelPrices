af9f594ca9ffe6c9545c04d197d977bcccc09bcd
whizzzkid <me@nishantarora.in>
2020-07-16 03:34:26 -0600

    Twitter/interfaces/OAuthParams: fixed

--- src/Twitter/interfaces/OAuthParams.ts
+++ src/Twitter/interfaces/OAuthParams.ts
@@ -1,9 +1,9 @@
 export interface OAuthParams {
-    oauth_signature: string;
     oauth_consumer_key: string;
-    oauth_token: string;
-    oauth_timestamp: string;
-    oauth_signature_method: string;
-    oauth_version: string;
     oauth_nonce: string;
+    oauth_signature_method: string;
+    oauth_signature: string;
+    oauth_timestamp: string;
+    oauth_token: string;
+    oauth_version: string;
 }
